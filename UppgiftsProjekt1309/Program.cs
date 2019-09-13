8cbbc07d37753b926b18732abcc43cef3871cfd5
Simpan1001 <simon.springer@edu.huddinge.se>
2019-09-13 13:53:40 +0200

    Uppgift 1 (nästan) klar

--- UppgiftsProjekt1309/Program.cs
+++ UppgiftsProjekt1309/Program.cs
@@ -10,6 +10,36 @@ namespace UppgiftsProjekt1309
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Please type a sentence.");
+            String phrase = Console.ReadLine();
+            Console.WriteLine();
+
+            int e = phrase.Length;
+            int letterCount = 0;
+            int wordCount = 1;
+            int[] list = new int[150];
+
+
+            for (int i = 0; i < e; i++) {
+
+                if (phrase[i] == ' ')
+                {
+                    wordCount++;
+                    Console.Write("(" + letterCount + ")\n");
+                    letterCount = 0;
+                }
+                else {
+                    Console.Write(phrase[i]);
+                    letterCount++;
+
+                }
+                
+            }
+
+            Console.WriteLine("\n\n" + wordCount);
+            Console.ReadLine();
+
         }
     }
 }
