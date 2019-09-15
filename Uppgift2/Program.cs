098ff5312b1b5cc4d9fd6ed94a709a6a52211c7e
Simpan1001 <simon.springer@edu.huddinge.se>
2019-09-15 12:27:18 +0200

    Uppgift 2 klar

--- Uppgift2/Program.cs
+++ Uppgift2/Program.cs
@@ -19,18 +19,44 @@ namespace Uppgift2
             int a = 0;
 
             for (int i = 0; i < e; i++) {
-
                 if (mening[i] == ' ')
                 {
+                    //Console.WriteLine(list[a]);
                     a++;
+                }
+                else if (i == e - 1) {
+                    list[a] += mening[i];
+                    //Console.WriteLine(list[a]);
                 }
                 else {
                     list[a] += mening[i];
                 }
-
-                
-
             }
+
+            String temp;
+            Boolean sorted = false;
+
+            while (!sorted) {
+                sorted = true;
+                for (int i = 0; i <= a- 1; i++) {
+                    if (list[i].Length > list[i+1].Length) {
+                        temp = list[i];
+                        list[i] = list[i + 1];
+                        list[i + 1] = temp;
+                        sorted = false;
+                    }
+                }
+            }
+
+            for (int i = 0; i <= a; i++) {
+                Console.WriteLine(list[i]);
+            }
+            Console.WriteLine();
+            for (int i = a; i >= 0; i--) {
+                Console.WriteLine(list[i]);
+            }
+
+            Console.ReadLine();
 
         }
     }
