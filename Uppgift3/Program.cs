7c17ba7a51157464c5b466337ed0e4d3bde61eb1
Simpan1001 <simon.springer@edu.huddinge.se>
2019-09-17 16:25:47 +0200

    Almost done with nr3

--- Uppgift3/Program.cs
+++ Uppgift3/Program.cs
@@ -11,70 +11,99 @@ namespace Uppgift3
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Var snäll och mata in en mening.");
-            string fras1 = Console.ReadLine();
-            string fras = fras1.ToLower();
-            string[] word = fras.Split(' ');
+            System.Collections.ArrayList MyList = new System.Collections.ArrayList();
+            int[] frequency = new int[MyList.Count];
+
+            Console.WriteLine("Please input a sentence.");
+            string mening = Console.ReadLine();
+            string[] temp = mening.Split(' ');
             Console.WriteLine();
 
-            int wordCount = 1;
-            foreach (char a in fras)
+            for (int i = 0; i < temp.Length; i++)
             {
-                if (a == ' ')
-                {
-                    wordCount++;
-                }
+                MyList.Add(temp[i]);
+            }
+           
+            string[] wordCounter = new string[MyList.Count];
+
+            for (int i = 0; i < wordCounter.Length; i++)
+            {
+                wordCounter[i] = "1";
             }
 
-            int[] wordAmount = new int[wordCount];
-            for (int i = 0; i < word.Length; i++)
+            if (String.Equals(MyList[0], MyList[1]))
             {
-                wordAmount[i] = 0;
+                wordCounter[0]+=1;
             }
 
-            int[] usage = new int[wordCount];
-            //Hmm, räkna varje gång ett ord förekommer... Hmmm....
-            for (int j = 0; j < word.Length; j++)
+            for (int j = 0; j < MyList.Count; j++)
             {
-                for (int i = 0; i < word.Length; i++)
+                for (int i = 0; i < MyList.Count; i++)
                 {
-                    if (word[j] == word[i])
+                    if (String.Equals(MyList[j], MyList[i]) && i != j && MyList[j] != "" && MyList[i] != "")
                     {
-                        usage[j]++;
+                        MyList[i] = "";
+                        wordCounter[i] = "";
+                        wordCounter[j] += 1;
                     }
                 }
-
-                if (wordAmount[j] < 1)
-                {
-                    Console.WriteLine("{0} ({1})", word[j], usage[j]);
-                    wordAmount[j]++;
-                }
             }
 
+            int c = 0;
+            int b = 0;
+            for (int i = 0; i < MyList.Count; i++)
+            {
+                foreach (char a in wordCounter[i])
+                {
+                    b++;
+                }
+                if (b != 0)
+                {
+                    c++;
+                    Console.WriteLine(MyList[i] + " (" + b + ")");
+                }
+                b = 0;
+            }
+
+            int[] order = new int[c];
+            int d = 0;
+            for (int i = 0; i < order.Length; i++)
+            {
+                if (wordCounter[i].Count() == 0)
+                {
+                    d++;
+                }
+                order[i] = wordCounter[d].Count();
+                d++;
+            }
+
+            System.Collections.ArrayList tempy = new System.Collections.ArrayList();
+
+            for (int i = 0; i < MyList.Count; i++)
+            {
+                tempy.Add(" ");
+            }
+
+            int temporary = 0;
+            for (int i = 0; i <= order.Length - 2; i++)
+            {
+                if (order[i] > order[i+1])
+                {
+                    temporary = order[i + 1];
+                    order[i + 1] = order[i];
+                    order[i] = temporary;
+
+                    tempy[0] = MyList[i + 1];
+                    MyList[i + 1] = MyList[i];
+                    MyList[i] = tempy[0];
+                }
+            }
             Console.WriteLine();
-            string temp = "";
-            Boolean sorted = false;
-
-                while (!sorted)
-                {
-                    sorted = true;
-                    for (int i = 0; i <= word.Length-1; i++)
-                    {
-                        if (usage[i] > usage[i + 1])
-                        {
-                            temp = word[i];
-                            word[i] = word[i + 1];
-                            word[i + 1] = temp;
-                            sorted = false;
-                        }
-                    }
-                }
-
-                Console.WriteLine("Continue");
-            for (int i = 0; i < word.Length; i++)
+            for (int i = 0; i <= MyList.Count-1; i++)
             {
-                Console.WriteLine(word[i]);
+                    Console.WriteLine(MyList[i]);
             }
+
             Console.ReadKey();
         }
     }
