1b1321485443ebd4c995cb7063c4ef2829e5abdc
Simpan1001 <simon.springer@edu.huddinge.se>
2019-09-19 16:58:28 +0200

    Uppgift4 nearly done

--- Uppgift4/Program.cs
+++ Uppgift4/Program.cs
@@ -11,22 +11,23 @@ namespace Uppgift4
         static void Main(string[] args)
         {
             //Låt användaren mata in en mening.
-            //Skriv ut varje unik bokstav på en egen rad, och ange hur ofta den förekommer;
+            //Skriv ut varje unik bokstav på en egen rad, och ange hur ofta den förekommer
 
             Console.WriteLine("Please input a sentence.");
-            //string phrase = Console.ReadLine();
-            string temp = "Hello world";
+            string temp = Console.ReadLine();
+            //string temp = "Hello world";
             string phrase = temp.ToLower();
             char[] letters = new char[phrase.Length];
             int[] counter = new int[26];//26 letters of the english alphabet
 
+            //This loop assigns every value in counter the startingvalue of 0 \/
             for (int i = 0; i < counter.Length; i++)
             {
                 counter[i] = 0;
             }
+            //This loop assigns every value in counter the startingvalue of 0 /\
 
 
-            Console.WriteLine();
             //This loop assigns each letter in "phrase" to the array \/
             for (int i = 0; i < phrase.Length; i++)
             {
@@ -34,18 +35,9 @@ namespace Uppgift4
             }
             //This loop assigns each letter in "phrase" to the array /\
 
-            //This loop only prints out the letters on the screen. (Removeable) \/
-            foreach (char s in letters) 
-            {
-                Console.Write(s);
-                if (s == ' ')
-                {
-                    Console.Write("\t");
-                }
-            }
-            //This loop only prints out the letters on the screen. (Removeable) /\
 
             Console.WriteLine("\n");
+            //This loop counts the occurence of every character \/
             for (int j = 0; j < letters.Length; j++) {
 
                 for (int i = 0; i < letters.Length; i++) {
@@ -54,8 +46,39 @@ namespace Uppgift4
                     {
                         counter[j]++;
                     }
+                }   
+            }
+            //This loop counts the occurence of every character /\
+
+
+            int f = 0;
+            int e = 1;
+            for (int i = 0; i < letters.Length; i++)
+            {
+                if (letters[i] != ' ')
+                {
+                    if (counter[i] > 1)//This if-algorithm is wrong somewhere...
+                    {
+                        f = counter[i];
+                        while (e < f)
+                        {
+                            for (int k = letters.Length; k >= 0; k--)
+                            {
+                                if (counter[k] == counter[i] && k != i)
+                                {
+                                    letters[k] = ' ';
+                                    counter[k] = 0;
+                                    e++;
+                                }
+                            }
+                        }
+                        e = 1;
+                    }//End of if-algorithm
+                    if (letters[i] != ' ' && counter[i] != 0)
+                    {
+                        Console.WriteLine(" {0} ({1})", letters[i], counter[i]);
+                    }
                 }
-                Console.WriteLine(" {0} ({1})", letters[j], counter[j]);
             }
 
             Console.ReadKey();
